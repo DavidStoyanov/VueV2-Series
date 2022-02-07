236df2e30518928da0a344e78a88b6a7862af28c
DavidStoyanov <david_st1@abv.bg>
2022-02-07 14:48:38 +0200

    AddBlog v-model

--- VueCLI/vuejs-playlist/src/App.vue
+++ VueCLI/vuejs-playlist/src/App.vue
@@ -1,64 +1,41 @@
 <template>
   <div id="app">
-
-    <keep-alive>
-      <component v-bind:is="component"></component>
-    </keep-alive>
-
-    <button v-on:click="switchFrom()">Switch Form</button>
+    
+    <add-blog></add-blog>
 
   </div>
 </template>
 
 <script>
-import FormOne from './components/FormOne.vue';
-import FormTwo from './components/FormTwo.vue';
+import AddBlog from "./components/AddBlog.vue";
 
 export default {
   components: {
-    'form-one': FormOne,
-    'form-two': FormTwo
+    'add-blog': AddBlog
   },
   data() {
     return {
-      component: "form-one"
+      
     }
   },
   methods: {
-    handleSubmit: function() {
-      
-    },
-    switchFrom: function() {
-      this.component = this.component === "form-one"
-        ? "form-two" : "form-one"
-    }
+    
   },
   computed: {
-    
+
   }
-}
+};
 </script>
 
 <style>
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
-  #app {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
+#app {
+  margin: 4rem;
+}
 
-  #app > * {
-    display: block;
-    width: 100%;
-  }
-
-  button {
-    max-width: 10rem;
-    align-self: center;
-  }
 </style>
